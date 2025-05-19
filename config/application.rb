48901aad1af4993045891c74a645cb2d10ca279a
Phil Mills <philliprmills@gmail.com>
2025-05-19 16:44:02 -0400

    sprockets removal again...

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require_relative "boot"
 require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
