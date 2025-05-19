dfef591d1ebf4340bacd4ae8a565dc3a7f372907
Phil Mills <philliprmills@gmail.com>
2025-05-19 16:47:07 -0400

    activestorage

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative "boot"
 require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
+require "active_storage/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
