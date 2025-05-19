e5bdd86da9043edbd5186ad28ca7ad3824314651
Phil Mills <philliprmills@gmail.com>
2025-05-19 17:19:17 -0400

    actionmailer x

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative "boot"
 # Only include the frameworks you need:
 require "action_controller/railtie"
 require "action_view/railtie"
-# Do NOT require "active_record/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
